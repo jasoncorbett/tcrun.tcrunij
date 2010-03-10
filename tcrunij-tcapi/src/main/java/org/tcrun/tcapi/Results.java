552f5081801b305ae9516499723cec3a098986c8
Jaron Jones <jaron.jones@gmail.com>
2010-03-10 09:15:40 -0700

    Added an enum for Test Case result types

---
+++ tcrunij-tcapi/src/main/java/org/tcrun/tcapi/Results.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.tcrun.tcapi;
+
+/**
+ *
+ * @author jaron
+ */
+public enum Results {
+    PASS,
+    FAIL,
+    SCRIPT_ERROR,
+    NOT_EXECUTED
+
+}
