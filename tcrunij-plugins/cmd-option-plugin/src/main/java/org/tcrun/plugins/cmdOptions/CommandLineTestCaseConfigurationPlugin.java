a3d20615b0d7e3c87b48ffffad7a4f5ab0b9674e
Jason Corbett <Jason_Corbett@intuit.com>
2010-03-26 10:39:26 -0600

    the command line test case configuration plugin is now fully functional

--- tcrunij-plugins/cmd-option-plugin/src/main/java/org/tcrun/plugins/cmdOptions/CommandLineTestCaseConfigurationPlugin.java
+++ tcrunij-plugins/cmd-option-plugin/src/main/java/org/tcrun/plugins/cmdOptions/CommandLineTestCaseConfigurationPlugin.java
@@ -4,24 +4,42 @@
  */
 package org.tcrun.plugins.cmdOptions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.tcrun.api.TCRunContext;
 import org.tcrun.api.plugins.CommandLineConsumerPlugin;
 import org.tcrun.api.plugins.CommandLineOptionPlugin;
+import org.tcrun.api.plugins.ConfigurationOverridePlugin;
 import org.tcrun.api.ImplementsPlugin;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 
 /**
  *
  * @author jcorbett
  */
-@ImplementsPlugin({CommandLineConsumerPlugin.class, CommandLineOptionPlugin.class})
-public class CommandLineTestCaseConfigurationPlugin implements CommandLineConsumerPlugin, CommandLineOptionPlugin
+@ImplementsPlugin({CommandLineConsumerPlugin.class, CommandLineOptionPlugin.class, ConfigurationOverridePlugin.class})
+public class CommandLineTestCaseConfigurationPlugin implements CommandLineConsumerPlugin, CommandLineOptionPlugin, ConfigurationOverridePlugin
 {
+	private Map<String,String> config;
+	private static XLogger logger = XLoggerFactory.getXLogger(CommandLineTestCaseConfigurationPlugin.class);
 
 	public void consumeCommandLineOptions(CommandLine options)
 	{
-		options.getOptionProperties("o").list(System.out);
+		logger.debug("Gathering data from command line options.");
+		config = new HashMap<String, String>();
+		Properties props = options.getOptionProperties("o");
+		logger.debug("There are '{}' options obtained from the command line for test cases.", props.size());
+		for(Object key :props.keySet())
+		{
+			String value = (String)props.get(key);
+			logger.debug("Parsed command line option (for test case): '{}' with value '{}'.", key, value);
+			config.put((String)key, value);
+		}
 	}
 
 	public String getPluginName()
@@ -37,5 +55,11 @@ public class CommandLineTestCaseConfigurationPlugin implements CommandLineConsum
                                  .withDescription("Set an option for the test case.")
 				 .withLongOpt("option")
                                  .create("o"));
+	}
+
+	public void handleConfiguration(TCRunContext context)
+	{
+		logger.debug("Adding '{}' test case configuration items to the context.", config.size());
+		context.getTestCaseConfiguration().putAll(config);
 	}
 }
