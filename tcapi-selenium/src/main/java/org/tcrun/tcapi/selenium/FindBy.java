5f0a0849c993f12be4a9431c98677d33703d2e9c
sharonlambson <sharonlambson@gmail.com>
2010-10-14 14:16:24 -0600

    there were some import statements that I thought i needed but do not so I am removing them.
    He: Enter commit message.  Lines beginning with 'HG:' are removed.

--- tcapi-selenium/src/main/java/org/tcrun/tcapi/selenium/FindBy.java
+++ tcapi-selenium/src/main/java/org/tcrun/tcapi/selenium/FindBy.java
@@ -1,10 +1,6 @@
 package org.tcrun.tcapi.selenium;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import java.util.List;
-import org.openqa.selenium.SearchContext;
-import java.lang.String;
 
 /**
  *
