27e0467867b3db6b80911beea17e53919a169aad
sharonlambson <sharonlambson@gmail.com>
2011-03-01 12:05:09 -0700

    Adding a switchToWindowByURLContainsAdding a switchToWindowByURLContainsAdding a switchToWindowByURLContainsAdding a switchToWindowByURLContainsAdding a switchToWindowByURLContains

--- tcapi-selenium/src/main/java/org/tcrun/tcapi/selenium/WebDriverWrapper.java
+++ tcapi-selenium/src/main/java/org/tcrun/tcapi/selenium/WebDriverWrapper.java
@@ -383,17 +383,32 @@ public interface WebDriverWrapper
 	 */
 	public void switchToWindowByHandle(String windowHandle);
 
+        /**
+	 * Switch to the browser window that contains the partial URL string.  Wait a maximum of the default
+	 * timeout for the select list to exist.
+	 * @param partialWindowURL Part of the url of the browser window you want to switch to.
+	 */
+	public void switchToWindowByURLContains(String partialWindowURL);
+
+	/**
+	 * Switch to the browser window that contains the partial URL string.  Wait the specified timeout
+	 * for the switch to window to be successful
+	 * @param partialWindowURL Part of the url of the browser window you want to switch to.
+	 * @param timeout The maximum amount of time to wait for the element to exist in seconds.
+	 */
+	public void switchToWindowByURLContains(String windowURL, int timeout);
+
 	/**
 	 * Switch to the browser window that contains the specified URL or partial URL.  Wait a maximum of the default
 	 * timeout for the select list to exist.
-	 * @param windowURL The url or partial url of the browser window you want to switch to.
+	 * @param windowURL The url of the browser window you want to switch to.
 	 */
 	public void switchToWindowByURL(String windowURL);
 
 	/**
 	 * Switch to the browser window that contains the specified URL or partial URL.  Wait the specified timeout
 	 * for the switch to window to be successful
-	 * @param windowURL The url or partial url of the browser window you want to switch to.
+	 * @param windowURL The url of the browser window you want to switch to.
 	 * @param timeout The maximum amount of time to wait for the element to exist in seconds.
 	 */
 	public void switchToWindowByURL(String windowURL, int timeout);
