5f88d4583d568206b782c44f70b8c0bf1a9c0de5
SharonLambson <SharonLambson@code.google.com>
2013-06-28 09:06:44 -0600

    Copying the files to a temp directory in the users home directory before executing to get around long path issues on Windows

--- tcrunij-packaging/src/main/template/tcrunij.bat
+++ tcrunij-packaging/src/main/template/tcrunij.bat
@@ -1,10 +1,18 @@
- @echo off
- rem This magic code found on http://stackoverflow.com/questions/524081/bat-file-to-create-java-classpath
- setLocal EnableDelayedExpansion
+@echo off
+rem This magic code found on http://stackoverflow.com/questions/524081/bat-file-to-create-java-classpath
+setLocal EnableDelayedExpansion
+
+rem We copy the files to a temp directory in the users home directory to handle long paths on Windows
+echo "Loading jar files....." 
+mkdir %HOMEPATH%\temptcrunij >nul 2>&1
+xcopy /S /E /Y * %HOMEPATH%\temptcrunij\ >nul 2>&1
+
  set CLASSPATH="
- for /R ./lib %%a in (*.jar) do (
+ for /R %HOMEPATH%/temptcrunij/lib %%a in (*.jar) do (
    set CLASSPATH=!CLASSPATH!;%%a
  )
  set CLASSPATH=!CLASSPATH!"
+
+echo "Running tcrunij now!"
 
 java -Dlogback.configurationFile=./conf/logging-config.xml org.tcrun.cmd.Main %*
