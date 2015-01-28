5ad892d37cde5cbdec3cf1acbbf786eb29155157
Jason Corbett <jasoncorbett@gmail.com>
2015-01-28 13:52:30 -0700

    Putting flowbee into selenium-wrapper

---
+++ selenium-wrapper/src/main/java/org/tcrun/selenium/flowbee/KnownPageTimeoutException.java
@@ -0,0 +1,12 @@
+package org.tcrun.selenium.flowbee;
+
+/**
+ *
+ * @author slambson
+ */
+public class KnownPageTimeoutException extends Exception
+{
+          public KnownPageTimeoutException(String message) {
+             super(message);
+          }
+}
