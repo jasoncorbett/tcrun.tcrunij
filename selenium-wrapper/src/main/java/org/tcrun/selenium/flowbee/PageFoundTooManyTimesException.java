c35fff9486179b18820ac5c48d62c72b94d01c48
Jason Corbett <jasoncorbett@gmail.com>
2015-01-28 13:52:30 -0700

    Putting flowbee into selenium-wrapper

---
+++ selenium-wrapper/src/main/java/org/tcrun/selenium/flowbee/PageFoundTooManyTimesException.java
@@ -0,0 +1,8 @@
+package org.tcrun.selenium.flowbee;
+
+public class PageFoundTooManyTimesException extends Exception
+{
+          public PageFoundTooManyTimesException(String message) {
+             super(message);
+          }
+}
